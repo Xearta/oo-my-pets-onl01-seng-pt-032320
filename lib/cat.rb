ff88af0c7073207f97f23c4e853bdc7ec6d57bf3
Tanner Townsend <rsmcwow@gmail.com>
2020-04-06 20:23:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,17 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all = []
+
   def initialize(name,owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    save
   end
+  
+  def save
+    @@all << self
+  end
+  
 end
